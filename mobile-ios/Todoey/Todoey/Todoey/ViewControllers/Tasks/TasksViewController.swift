a026513bb29034a87c2230be36f898611dfe0dad
robertwitt <robert_witt@icloud.com>
2023-03-31 16:40:18 +0200

    Refactor handling after editing task

--- mobile-ios/Todoey/Todoey/Todoey/ViewControllers/Tasks/TasksViewController.swift
+++ mobile-ios/Todoey/Todoey/Todoey/ViewControllers/Tasks/TasksViewController.swift
@@ -50,25 +50,7 @@ class TasksViewController: FUIFormTableViewController, SAPFioriLoadingIndicator 
     }
     
     private func updateTaskListAfterUpdating(task: Tasks) {
-        let index = taskList.tasks.firstIndex { $0.id == task.id }
-        
-        if !taskList.shouldList(task: task), let index = index {
-            // Delete rows
-            taskList.tasks.remove(at: index)
-            tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
-            postTaskListUpdatedNotification()
-            return
-        }
-        
-        if let index = index {
-            // Update row
-            taskList.tasks[index] = task
-            tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
-        } else if taskList.shouldList(task: task) {
-            // Add row
-            taskList.tasks.append(task)
-            tableView.insertRows(at: [IndexPath(row: taskList.tasks.count - 1, section: 0)], with: .automatic)
-        }
+        taskList.onTaskUpdated(task, postProcessor: self)
         postTaskListUpdatedNotification()
     }
     
@@ -76,12 +58,7 @@ class TasksViewController: FUIFormTableViewController, SAPFioriLoadingIndicator 
         guard let task = notification.userInfo?["Task"] as? Tasks else {
             return
         }
-        let index = taskList.tasks.firstIndex { $0.id == task.id }
-        guard let index = index else {
-            return
-        }
-        taskList.tasks.remove(at: index)
-        tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+        taskList.onTaskRemoved(task, postProcessor: self)
         postTaskListUpdatedNotification()
     }
     
@@ -166,6 +143,22 @@ extension TasksViewController: TaskEditViewControllerDelegate {
                 }
             }
         }
+    }
+    
+}
+
+extension TasksViewController: TaskEditPostProcessing {
+    
+    func afterInsert(at rowIndex: Int) {
+        tableView.insertRows(at: [IndexPath(row: rowIndex, section: 0)], with: .automatic)
+    }
+    
+    func afterUpdate(at rowIndex: Int) {
+        tableView.reloadRows(at: [IndexPath(row: rowIndex, section: 0)], with: .automatic)
+    }
+    
+    func afterDelete(at rowIndex: Int) {
+        tableView.deleteRows(at: [IndexPath(row: rowIndex, section: 0)], with: .automatic)
     }
     
 }
