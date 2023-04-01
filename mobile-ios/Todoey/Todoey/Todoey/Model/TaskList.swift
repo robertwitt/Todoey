e498b2c89f9994e159ad1eb53e7ebd1f64cc7aaa
robertwitt <robert_witt@icloud.com>
2023-04-01 18:56:46 +0200

    Refactor updating tasks

--- mobile-ios/Todoey/Todoey/Todoey/Model/TaskList.swift
+++ mobile-ios/Todoey/Todoey/Todoey/Model/TaskList.swift
@@ -21,49 +21,10 @@ protocol TaskList {
     var isEditable: Bool { get }
     var tasks: [Tasks] { get set }
     
-    func shouldList(task: Tasks) -> Bool
     func newTask() -> Tasks
-//    mutating func onTaskUpdated(_ task: Tasks, postProcessor: TaskEditPostProcessing?)
-//    mutating func onTaskRemoved(_ task: Tasks, postProcessor: TaskEditPostProcessing?)
-//    
-}
-
-extension TaskList {
-    
-    mutating func onTaskUpdated(_ task: Tasks, postProcessor: TaskEditPostProcessing?) {
-        let index = tasks.firstIndex { $0.id == task.id }
-        
-        if !shouldList(task: task), let index = index {
-            // Delete rows
-            tasks.remove(at: index)
-            DispatchQueue.main.async {
-                postProcessor?.afterDelete(at: index)
-            }
-        } else if let index = index {
-            // Update row
-            tasks[index] = task
-            DispatchQueue.main.async {
-                postProcessor?.afterUpdate(at: index)
-            }
-        } else if shouldList(task: task) {
-            // Add row
-            tasks.append(task)
-            let index = self.tasks.count - 1
-            DispatchQueue.main.async {
-                postProcessor?.afterInsert(at: index)
-            }
-        }
-    }
-    
-    mutating func onTaskRemoved(_ task: Tasks, postProcessor: TaskEditPostProcessing?) {
-        let index = tasks.firstIndex { $0.id == task.id }
-        guard let index = index else {
-            return
-        }
-        tasks.remove(at: index)
-        postProcessor?.afterDelete(at: index)
-    }
-    
+    func onTaskUpdated(_ task: Tasks, postProcessor: TaskEditPostProcessing?)
+    func onTaskRemoved(_ task: Tasks, postProcessor: TaskEditPostProcessing?)
+  
 }
 
 protocol TaskEditPostProcessing {
